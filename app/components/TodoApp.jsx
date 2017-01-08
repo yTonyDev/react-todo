5f27db96bf37bdcada8f6ccd525c85c4d5125c96
Tony Yiu <yTonyDev@gmail.com>
2017-01-07 22:26:03 -0500

    Refactor TodoSearch to work with redux store

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,7 +4,7 @@ var moment =require('moment');
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-var TodoSearch = require('TodoSearch');
+import TodoSearch from 'TodoSearch';
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
@@ -18,20 +18,6 @@ var TodoApp = React.createClass({
     componentDidUpdate: function(){
         TodoAPI.setTodos(this.state.todos);
     },
-    handleAddTodo: function(text){
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {
-                    id: uuid(),
-                    text: text,
-                    completed: false,
-                    createdAt: moment().unix(),
-                    completedAt: undefined
-                }
-            ]
-        });
-    },
     handleSearch: function(showCompleted, searchText){
         this.setState({
             showCompleted: showCompleted,
@@ -39,17 +25,15 @@ var TodoApp = React.createClass({
         });
     },
     render: function(){
-        var {todos, showCompleted, searchText} = this.state;
-        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div>
                 <h1 className="page-title">Todo App</h1>
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
                         <div className="container">
-                            <TodoSearch onSearch={this.handleSearch}/>
+                            <TodoSearch/>
                             <TodoList/>
-                            <AddTodo onAddTodo={this.handleAddTodo}/>
+                            <AddTodo/>
                         </div>
                     </div>
                 </div>
