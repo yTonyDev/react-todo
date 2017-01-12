f4eb72cb87a0e20eb572a1eff822579085d66901
Tony Yiu <yTonyDev@gmail.com>
2017-01-12 15:38:45 -0500

    update todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -11,14 +11,13 @@ var store = require('configureStore').configure();
 firebase.auth().onAuthStateChanged((user)=>{
     if(user){
         store.dispatch(actions.login(user.uid));
+        store.dispatch(actions.startAddTodos());
         hashHistory.push('/todos');
     } else {
         store.dispatch(actions.logout());
         hashHistory.push('/');
     }
 });
-
-store.dispatch(actions.startAddTodos());
 
 //Load foundation
 $(document).foundation();
